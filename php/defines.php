c0c5cbc1687a0a585280bd6a709fc110d4e19b23
Andrew Duthie <andrew@andrewduthie.com>
2015-12-03 10:03:05 -0500

    PHP: Fix formatting and improve detail
    Also resolves issue where redirect attempt to login page would have
    been made even after successful authorization.

--- php/defines.php
+++ php/defines.php
@@ -1,8 +1,12 @@
 <?php
 
-define ( 'CLIENT_ID', 1234 ); //TODO
-define ( 'CLIENT_SECRET', 'Your Secret' ); //TODO
-define ( 'LOGIN_URL', 'http://localhost/' ); //TODO
-define ( 'REDIRECT_URL', 'http://localhost/connected.php' ); //TODO
-define ( 'REQUEST_TOKEN_URL', 'https://public-api.wordpress.com/oauth2/token' );
-define ( 'AUTHENTICATE_URL', 'https://public-api.wordpress.com/oauth2/authenticate' );
+// TODO: Replace these with the values shown on your app's Manage Settings page
+// Visit: https://developer.wordpress.com/apps/
+define( 'CLIENT_ID', 1 );
+define( 'CLIENT_SECRET', 'Your Application Secret' );
+define( 'LOGIN_URL', 'http://localhost/' );
+define( 'REDIRECT_URL', 'http://localhost/connected.php' );
+
+// You do not need to change these settings
+define( 'REQUEST_TOKEN_URL', 'https://public-api.wordpress.com/oauth2/token' );
+define( 'AUTHENTICATE_URL', 'https://public-api.wordpress.com/oauth2/authenticate' );
